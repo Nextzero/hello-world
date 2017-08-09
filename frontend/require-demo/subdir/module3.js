9e645bdec5c11163942d82e6025665a63bbd7e29
Nextzero <77848041@qq.com>
2017-08-09 11:36:11 +0800

    11

---
+++ frontend/require-demo/subdir/module3.js
@@ -0,0 +1,10 @@
+define([], function(require, factory) {
+    'use strict';
+    console.log("这里是module3");
+    
+    return {
+        getName:function(){
+            return "module3";
+        }
+    }
+});
