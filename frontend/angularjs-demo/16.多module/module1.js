14ae26ff72fa037a8bc6c98ee24248e81cf06aeb
domi <77848041@qq.com>
2018-02-05 16:51:21 +0800

    多module指令

--- frontend/angularjs-demo/16.多module/module1.js
+++ frontend/angularjs-demo/16.多module/module1.js
@@ -8,3 +8,14 @@ app.service('myService', function(){
         name: 'huangchao'
     }
 })
+
+app.directive('myHello', function($http){
+    //自定义一个指令
+    //该函数为工厂函数，需要返回一个对象，对象包含指令的描述信息
+    //该函数支持注入(这里并不需要$http服务，只是用来展示'支持注入')
+    return {
+        restrict: 'EA',
+        //可以在template中指定模板内容，也可以使用templateUrl引用一个外部的html文件
+        template: '<a href="http://google.com">Click me to go to Google</a>'
+    };
+});
